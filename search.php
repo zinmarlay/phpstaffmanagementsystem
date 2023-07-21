4e4d5558ce2d2bf785e196be886195f6606d951e
ZinMarLwin <zimmar07@gmail.com>
2023-07-21 18:23:29 +0900

    安心に削除できるように設定をする

--- search.php
+++ search.php
@@ -12,12 +12,50 @@ $nameKana = '';
 $gender = '';
 $whereSql = '';
 $param = [];
+$errorMessage = '';
+$successMessage = '';
 
 //deleteUserのsubmit()を押すと、IDを取得できる
-if(mb_strtolower($_SERVER["REQUEST_METHOD"]) == "post"){
-  var_dump($_POST);
+if (mb_strtolower($_SERVER['REQUEST_METHOD']) === 'post') {
+  //trueならば削除ボタンが押されたということ
+  $isDelete = (isset($_POST['delete']) && $_POST['delete'] === '1') ? true : false;
+
+  if ($isDelete === true) {
+      //POSTされた社員番号の入力チェック
+      $deleteId = isset($_POST['id']) ? $_POST['id'] : '';
+      if ($deleteId === '') { //空白でないか
+          $errorMessage .= '社員番号が不正です。<br>';
+      } else if (!preg_match('/\A[0-9]{6}\z/', $deleteId)) { //6桁の数値か
+          $errorMessage .= '社員番号が不正です。<br>';
+      } else {
+          //存在する社員番号か
+          $sql = "SELECT COUNT(*) AS count FROM users WHERE id = :id";
+          $param = array("id" => $deleteId);
+          $stmt = $pdo->prepare($sql);
+          $stmt->execute($param);
+          $count = $stmt->fetch(PDO::FETCH_ASSOC);
+          if ($count['count'] === '0') {
+              $errorMessage .= '社員番号が不正です。<br>';
+          }
+      }
+
+      //入力チェックOK?
+      if ($errorMessage === '') {
+          $pdo->beginTransaction();
+          $sql = "DELETE FROM users WHERE id = :id";
+          $param = array("id" => $deleteId);
+          $stmt = $pdo->prepare($sql);
+          $stmt->execute($param);
+          $pdo->commit();
+          $successMessage = "削除完了しました。";
+      } else {
+          // エラー有り
+          echo $errorMessage;
+      }
+  }
 }
 
+$param = [];
 // 検索条件が指定されている
 if (isset($_GET['id']) && isset($_GET['name_kana'])) {
     $id = $_GET['id'];
@@ -45,7 +83,7 @@ if ($gender !== '') {
 }
 //件数取得SQLの実行
 $sql = "SELECT COUNT(*) As count FROM users where 1 = 1 {$whereSql}";
-// $param = [];
+
 $stmt = $pdo->prepare($sql);
 $stmt->execute($param);
 $count = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -108,7 +146,12 @@ $stmt->execute($param);
         </div>
       </form>
     </div>
-
+    <?php if ($errorMessage != "") :?>
+    <p class="error_message"><?php echo $errorMessage;?></p>
+    <?php endif;?>
+    <?php if ($successMessage != "") :?>
+    <p class="success_message"><?php echo $successMessage;?></p>
+    <?php endif;?>
     <?php //件数表示 ?>
     <div id="page_area">
       <div id="page_count"><?php echo $count["count"];?>件ヒットしました</div>
@@ -145,7 +188,7 @@ $stmt->execute($param);
             <td><?php echo htmlspecialchars($row["mail_address"]); ?></td>
             <td class="button_area">
             <button class="edit_button">編集</button>
-            <button class="delete_button" onclick="deleteUser(<?php echo htmlspecialchars($row["id"]); ?>);">削除</button>
+            <button class="delete_button" onclick="deleteUser('<?php echo htmlspecialchars($row["id"]); ?>');">削除</button>
             </td>
           </tr>
           <?php endwhile;?>
